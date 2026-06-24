5849a0924adbff006e19e089d8df478442b2cec6
Carlos Nihelton <carlos.nihelton@canonical.com>
2026-06-24 09:26:59 -0300

    Integration testing for the LC_* variables
    If systemd and cloud-init do nothing, we should have the C.UTF-8 locale,
    so printf should outut 0.500. With both ON but only WSL exporting LANG,
    we still should get that value. If our fix script exports LC_NUMERIC,
    only then we can get the expected "0,500".
    Source profile manually As we're actually running a script directly with
    `wsl.exe -- bash $script` we're not in a login shell, so sourcing
    profile is (likely) skipped.

--- test/basic-assertions.sh
+++ test/basic-assertions.sh
@@ -28,3 +28,24 @@ if [[ $(whoami) != "$EXPECTED_USER" ]]; then
 	echo "::error:: Default user doesn't match expected user '$EXPECTED_USER'."
 	exit 4
 fi
+
+# WSL only sets LANG from /etc/default/locale. We attempt to set the LC_* variables from the systemd
+# environment. This test assumes that cloud-init set locale to en_US.UTF-8 but LC_NUMERIC to
+# pt_BR.UTF-8 if systemd is ON (see .github/workflows/integration.yaml:15).
+# Without systemd and cloud-init the default locale would be C.UTF-8 so the expectations still hold.
+expected="C.UTF-8"
+systemd_running="OFF"
+if systemctl is-system-running --quiet; then
+	expected="pt_BR.UTF-8"
+	systemd_running="ON"
+fi
+if [[ "$LC_NUMERIC" != "$expected" ]]; then
+	echo "::error:: With systemd $systemd_running, expected LC_NUMERIC to be $expected but got $LC_NUMERIC."
+	echo "Expected locale:"
+	cat /etc/default/locale
+	echo "got locale:"
+	locale
+	echo "systemd environment:"
+	systemctl show-environment
+	exit 5
+fi
